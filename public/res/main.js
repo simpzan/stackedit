6362d604095d69fed9cb11243de4d4e33bca2af6
Samuel Zhang <simpzan@gmail.com>
2016-10-29 17:04:14 +0800

    addd pouchdb for note persistence.

--- public/res/main.js
+++ public/res/main.js
@@ -15,6 +15,7 @@ requirejs.config({
 		}
 	],
 	paths: {
+		pouchdb: 'bower-libs/pouchdb/dist/pouchdb',
 		jquery: 'bower-libs/jquery/jquery',
 		underscore: 'bower-libs/underscore/underscore',
 		crel: 'bower-libs/crel/crel',
